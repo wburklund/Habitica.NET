1971cdfdb8c130c0659cd4f4688c13d179c97537
Will Burklund <42753606+wburklund@users.noreply.github.com>
2020-04-12 14:52:35 -0500

    End test -warnAsError

--- Habitica.NET/Extensions.cs
+++ Habitica.NET/Extensions.cs
@@ -14,8 +14,6 @@ namespace Habitica.NET
             if (nameValueCollection == null) throw new ArgumentNullException(nameof(nameValueCollection));
             if (nameValueCollection.Count == 0) return "";
             
-            int testWarnAsError = 5;
-
             var keys = nameValueCollection.AllKeys;
             var pairs = keys.Select(k => new KeyValuePair<string, string>(k, nameValueCollection[k]));
             var parameters = pairs.Select(p => Uri.EscapeUriString(p.Key) + "=" + Uri.EscapeUriString(p.Value));
