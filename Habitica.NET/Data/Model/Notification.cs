7acb5ee10a026ffd781eeee40b5a218556e72073
Will Burklund <waburklund@gmail.com>
2020-04-13 18:20:37 -0500

    Clean up notifications

---
+++ Habitica.NET/Data/Model/Notification.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Will Burklund. Licensed under the MIT License.  See LICENSE in the project root for license information.
+
+using System;
+
+namespace Habitica.NET.Data.Model
+{
+    /// <summary>
+    /// Notifications are included in every Habitica response.
+    /// </summary>
+    public class Notification
+    {
+        /// <summary>
+        /// Gets or sets the ID of the notification.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the inner data of the notification. TODO: Add inner data models.
+        /// </summary>
+        public object Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the notification has been seen.
+        /// </summary>
+        public bool Seen { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the notification.
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
