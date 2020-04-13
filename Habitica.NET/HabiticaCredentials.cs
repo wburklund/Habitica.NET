0a50cdf1cebeaa2990b9fe7738e18ac29bd371a5
Will Burklund <waburklund@gmail.com>
2020-04-13 07:20:18 -0500

    Add comments to HabiticaCredentials.cs

--- Habitica.NET/HabiticaCredentials.cs
+++ Habitica.NET/HabiticaCredentials.cs
@@ -4,11 +4,26 @@ using System;
 
 namespace Habitica.NET
 {
+    /// <summary>
+    /// Habitica credentials object. Contains information on the current user as well as the application.
+    /// </summary>
     public class HabiticaCredentials
     {
+        /// <summary>
+        /// The application author's Habitica user ID.
+        /// </summary>
         public Guid AppAuthorUserId { get; set; }
+        /// <summary>
+        /// The application's name.
+        /// </summary>
         public string AppName { get; set; }
+        /// <summary>
+        /// The ID of the user for which requests will be made.
+        /// </summary>
         public Guid ApiUserId { get; set; }
+        /// <summary>
+        /// The API token of the user for which requests will be made.
+        /// </summary>
         public Guid ApiToken { get; set; }
 
         public HabiticaCredentials() { }
