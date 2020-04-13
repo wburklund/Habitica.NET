41833b817dfc477f37e986161f3249b2127f1cd4
Will Burklund <waburklund@gmail.com>
2020-04-13 17:41:35 -0500

    More HttpResponseException tests

--- Habitica.NET.UnitTests/TestHttpResponseException.cs
+++ Habitica.NET.UnitTests/TestHttpResponseException.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Will Burklund. Licensed under the MIT License.  See LICENSE in the project root for license information.
 
 using Habitica.NET.Exceptions;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -12,11 +13,56 @@ namespace Habitica.NET.UnitTests
         private static string bodyString = @"{""data"":""{}"", ""success"":true}";
 
         [Fact]
-        public async Task HttpResponseException_ThrowWithMessage_Succeeds()
+        public void ResponseProperty_Get_ReturnsProvidedResponse()
+        {
+            var response = GetTestResponse();
+            var ex = new HttpResponseException(response);
+            Assert.Equal(response, ex.Response);
+        }
+
+        [Fact]
+        public async Task DefaultConstructor_Invoke_Succeeds()
         {
             await Assert.ThrowsAsync<HttpResponseException>(() =>
             {
-                throw new HttpResponseException(GetTestResponse());
+                throw new HttpResponseException();
+            });
+        }
+
+        [Fact]
+        public async Task StringConstructor_Invoke_Succeeds()
+        {
+            await Assert.ThrowsAsync<HttpResponseException>(() =>
+            {
+                throw new HttpResponseException();
+            });
+        }
+
+        [Fact]
+        public async Task StringExceptionConstructor_Invoke_Succeeds()
+        {
+            await Assert.ThrowsAsync<HttpResponseException>(() =>
+            {
+                throw new HttpResponseException("Test string");
+            });
+        }
+
+
+        [Fact]
+        public async Task MessageConstructor_Invoke_Succeeds()
+        {
+            await Assert.ThrowsAsync<HttpResponseException>(() =>
+            {
+                throw new HttpResponseException("Test string", new Exception());
+            });
+        }
+
+        [Fact]
+        public async Task MessageExceptionConstructor_Invoke_Succeeds()
+        {
+            await Assert.ThrowsAsync<HttpResponseException>(() =>
+            {
+                throw new HttpResponseException(GetTestResponse(), new Exception());
             });
         }
 
