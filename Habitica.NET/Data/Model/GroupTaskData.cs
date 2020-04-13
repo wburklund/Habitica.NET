066334b5db0c6dd464ca8350bdd4d27d744f5d0a
Will Burklund <waburklund@gmail.com>
2020-04-13 18:29:13 -0500

    Delete empty model

--- Habitica.NET/Data/Model/GroupTaskData.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Will Burklund. Licensed under the MIT License.  See LICENSE in the project root for license information.
-
-namespace Habitica.NET.Data.Model
-{
-    public class GroupTaskData
-    {
-    }
-}
