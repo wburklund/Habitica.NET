97c47b73f7b8a8204c646a2fffd6619b782b9bf0
Will Burklund <waburklund@gmail.com>
2020-04-25 21:56:48 -0500

    Add ToJson extension

--- Habitica.NET.Tests/Extensions.cs
+++ Habitica.NET.Tests/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
@@ -8,6 +9,7 @@ namespace Habitica.NET.Tests
 {
     internal static class Extensions
     {
+        internal static string ToJson(this object obj) => JsonConvert.SerializeObject(obj);
         internal static HttpResponseMessage WrapResponseBody(this string body) => body.WrapResponseBody(HttpStatusCode.OK);
 
         internal static HttpResponseMessage WrapResponseBody(this string body, HttpStatusCode statusCode)
