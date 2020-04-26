dba7ccc76638b23bb9fda4cca31320661fdd2d2e
Will Burklund <waburklund@gmail.com>
2020-04-26 15:42:05 -0500

    Add ToQueryString test

---
+++ Habitica.NET.Tests/Unit/ExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using Xunit;
+
+namespace Habitica.NET.Tests.Unit
+{
+    public class ExtensionsTests
+    {
+        [Fact]
+        public void ToQueryString_EmptyCollection_ReturnsEmptyString()
+        {
+            var collection = new NameValueCollection();
+            Assert.Equal("", collection.ToQueryString());
+        }
+    }
+}
