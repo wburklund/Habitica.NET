e69d4afdd9cfc2aae8cbe240469c93692bbf2fb5
Will Burklund <waburklund@gmail.com>
2020-04-26 13:54:02 -0500

    Unit test assertions

---
+++ Habitica.NET.Tests.Unit/Data/TaskClientTests.cs
@@ -0,0 +1,52 @@
+﻿using Habitica.NET.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Habitica.NET.Tests.Unit.Data
+{
+    public class TaskClientTests
+    {
+        private Mock<ICoreClient> MockCoreClient = new Mock<ICoreClient>();
+        private TaskClient GetTaskClient() => new TaskClient(MockCoreClient.Object);
+
+        [Fact]
+        public void AddTaskTagAsync_EmptyTaskId_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>("taskId", () => GetTaskClient().AddTaskTagAsync(null, Guid.NewGuid()).Result);
+        }
+
+        [Fact]
+        public void AddTaskTagAsync_EmptyTagId_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>("tagId", () => GetTaskClient().AddTaskTagAsync("taskId", default).Result);
+        }
+
+        [Fact]
+        public void AddTaskChecklistItemAsync_EmptyTaskId_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>("taskId", () => GetTaskClient().AddTaskChecklistItemAsync(null, "itemText").Result);
+        }
+
+        [Fact]
+        public void AddTaskChecklistItemAsync_EmptyItemText_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>("itemText", () => GetTaskClient().AddTaskChecklistItemAsync("taskId", null).Result);
+        }
+
+        [Fact]
+        public void AssignUserTaskAsync_EmptyTaskId_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>("taskId", () => GetTaskClient().AssignUserTaskAsync(default, Guid.NewGuid()).Result);
+        }
+
+        [Fact]
+        public void AssignUserTaskAsync_EmptyAssignedUserId_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>("assignedUserId", () => GetTaskClient().AssignUserTaskAsync(Guid.NewGuid(), default).Result);
+        }
+    }
+}
