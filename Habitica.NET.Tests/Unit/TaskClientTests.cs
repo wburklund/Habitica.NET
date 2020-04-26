95c87164f5fdccf3d128ed386028cd63e88b419e
Will Burklund <waburklund@gmail.com>
2020-04-26 15:22:52 -0500

    That it's a demon

--- Habitica.NET.Tests/Unit/TaskClientTests.cs
+++ Habitica.NET.Tests/Unit/TaskClientTests.cs
@@ -1,10 +1,13 @@
-﻿using Habitica.NET.Interfaces;
+﻿using Habitica.NET.Data.Request;
+using Habitica.NET.Interfaces;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+
+using HTask = Habitica.NET.Data.Model.Task;
 
 namespace Habitica.NET.Tests.Unit.Data
 {
@@ -71,10 +74,83 @@ namespace Habitica.NET.Tests.Unit.Data
 
         [Theory]
         [ClassData(typeof(InvalidGuidGuid))]
-        public Task AssignUserTaskAsync_InvalidParameters_ThrowsArgumentException(Guid taskId, Guid assignedUserId)
+        public Task AssignUserTaskAsync_InvalidParameters_ThrowsArgumentException(Guid taskId, Guid userId)
         {
-            return Assert.ThrowsAsync<ArgumentException>(() => GetTaskClient().AssignUserTaskAsync(taskId, assignedUserId));
+            return Assert.ThrowsAsync<ArgumentException>(() => GetTaskClient().AssignUserTaskAsync(taskId, userId));
         }
 
+
+        [Theory]
+        [ClassData(typeof(InvalidGuidIEnumerableT<CreateTask>))]
+        public Task CreateChallengeTaskAsync_InvalidParameters_ThrowsArgumentException(Guid taskId, IEnumerable<CreateTask> tasks)
+        {
+            return Assert.ThrowsAsync<ArgumentException>(() => GetTaskClient().CreateChallengeTaskAsync(taskId, tasks));
+        }
+
+        private class InvalidGuidIEnumerableT<T> : TheoryData<Guid, IEnumerable<T>>
+            where T: new()
+        {
+            public InvalidGuidIEnumerableT()
+            {
+                Add(default, new List<T> { new T() });
+                Add(Guid.NewGuid(), null);
+                Add(Guid.NewGuid(), new List<T> { });
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(InvalidGuidIEnumerableT<CreateTask>))]
+        public Task CreateGroupTaskAsync_InvalidParameters_ThrowsArgumentException(Guid taskId, IEnumerable<CreateTask> tasks)
+        {
+            return Assert.ThrowsAsync<ArgumentException>(() => GetTaskClient().CreateGroupTaskAsync(taskId, tasks));
+        }
+
+        [Theory]
+        [ClassData(typeof(InvalidIEnumerableT<CreateUserTask>))]
+        public Task CreateUserTaskAsync_InvalidParameters_ThrowsArgumentException(IEnumerable<CreateUserTask> tasks)
+        {
+            return Assert.ThrowsAsync<ArgumentException>(() => GetTaskClient().CreateUserTaskAsync(tasks));
+        }
+
+        private class InvalidIEnumerableT<T> : TheoryData<IEnumerable<T>>
+            where T : new()
+        {
+            public InvalidIEnumerableT()
+            {
+                Add(null);
+                Add(new List<T> { });
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(InvalidStringGuid))]
+        public Task DeleteTaskChecklistItemAsync_InvalidParameters_ThrowsArgumentException(string taskId, Guid itemId)
+        {
+            return Assert.ThrowsAsync<ArgumentException>(() => GetTaskClient().DeleteTaskChecklistItemAsync(taskId, itemId));
+        }
+
+        [Theory]
+        [ClassData(typeof(InvalidStringGuid))]
+        public Task DeleteTaskTagAsync_InvalidParameters_ThrowsArgumentException(string taskId, Guid tag)
+        {
+            return Assert.ThrowsAsync<ArgumentException>(() => GetTaskClient().DeleteTaskTagAsync(taskId, tag));
+        }
+
+        [Theory]
+        [ClassData(typeof(InvalidString))]
+        public Task DeleteTaskAsync_InvalidParameters_ThrowsArgumentException(string taskId)
+        {
+            return Assert.ThrowsAsync<ArgumentException>(() => GetTaskClient().DeleteTaskAsync(taskId));
+        }
+
+        private class InvalidString : TheoryData<string>
+        {
+            public InvalidString()
+            {
+                Add(null);
+                Add("");
+                Add(" ");
+            }
+        }
     }
 }
